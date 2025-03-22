422e3bda146ce5dbac9990e853898187e84b050b
LucasDev <lucascaique966@gmail.com>
2025-03-21 21:26:21 -0300

    Add files via upload

---
+++ src/redes_trabajos.js
@@ -0,0 +1,59 @@
+const REDES_SOCIALES = {
+  github: 'https://github.com/LucasCaiqueDev',
+  linkedin: 'https://www.linkedin.com/in/lucascaiquedev/',
+  correo: 'lucascaique966@gmail.com'
+}
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+const TRABAJOS = [
+  {
+    nombre: 'Cubo Rubik',
+    fecha: '18 - Nov - 2025',
+    descripcion:
+      "Um projeto pessoal, uma animação de um cubo mágico em 3D usando apenas html e css.",
+    website: 'https://LucasCaiquePortifolio.io/3D-Rubiks-Cube/',
+    github: 'https://github.com/jaenfigueroa/3D-Rubiks-Cube',
+    imagen: './assets/proyectos/cubo_rubik.png'
+  },
+  {
+    nombre: 'Elo Store eCommerce',
+    fecha: '21 - Março - 2025',
+    descripcion:
+      'Site onde são vendidos diferentes pratos de comida de diferentes preços e sabores.',
+    website: 'https://elo-store-e-commerce.vercel.app/',
+    github: 'https://github.com/LucasCaiqueDev/Elo-Store-E-commerce',
+    imagen: './assets/proyectos/ecommerce.png'
+  },
+  {
+    nombre: 'Web Portfolio',
+    fecha: '15 - Dec - 2025',
+    descripcion:
+      'Site onde são vendidos diferentes pratos de comida de diferentes preços e sabores.',
+    website: 'https://LucasCaiquePortifolio.io/portifoliolucasdev.github.io',
+    github: 'https://github.com/LucasCaiqueDev/portifoliolucasdev.github.io',
+    imagen: './assets/proyectos/web_portfolio.png'
+  },
+  {
+    nombre: 'Manage Movies List app',
+    fecha: '15 - Dec - 2025',
+    descripcion:
+      'Aplicativo para salvar listas de filmes, você pode editá-los, movê-los, excluí-los, pesquisá-los, etc.',
+    website: 'https://manage-movie-list.netlify.app/',
+    github: 'https://github.com/LucasCaiqueDev/manage-movie-list',
+    imagen: './assets/proyectos/movies.png'
+  },
+  {
+    nombre: 'PokeZone',
+    fecha: '3 - Jan - 2025',
+    descripcion:
+      'Este projeto é um site com tema Pokémon criado com React.js e a API PokeAPI.',
+    website: 'https://pokezone.LucasCaiqueDev.com/',
+    github: 'https://github.com/LucasCaiqueDev/PokeZone',
+    imagen: './assets/proyectos/pokezone.png'
+  }
+]
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
